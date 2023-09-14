6c13693c81a18f40f26f6ec8f2e77f1ec7a5efcb
garvp <garvp@GARVS-SP7>
2023-09-14 13:51:42 +1200

    Add project files.

---
+++ Circuits/Gate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circuits
+{
+    public class Gate
+    {
+        protected int left;
+        protected int top;
+        protected const int WIDTH = 40;
+        protected const int HEIGHT = 40;
+        protected const int GAP = 10;
+
+        protected List<Pin> pins = new List<Pin>();
+
+        protected bool selected = false;
+
+        public bool Selected
+        {
+            get { return selected; }
+            set { selected = value; }
+        }
+
+        public int Left
+        {
+            get { return left; }
+        }
+
+        public int Top
+        {
+            get { return top; }
+        }
+
+        public List<Pin> Pins
+        {
+            get { return pins; }
+        }
+
+        public bool IsMouseOn(int x, int y)
+        {
+            if (left <= x && x < left + WIDTH && top <= y && y < top + HEIGHT)
+                return true;
+            else
+                return false;
+        }
+
+        public virtual void Draw(Graphics paper)
+        {
+            if (selected)
+            {
+                paper.DrawImage(Properties.Resources.AndGateAllRed, Left, Top);
+            }
+            else
+            {
+                paper.DrawImage(Properties.Resources.AndGate, Left, Top);
+            }
+
+            foreach (Pin p in pins)
+                p.Draw(paper);
+        }
+
+         public void MoveTo(int x, int y)
+        {
+            left = x;
+            top = y;
+
+            pins[0].X = x - GAP;
+            pins[0].Y = y + GAP;
+            pins[1].X = x - GAP;
+            pins[1].Y = y + HEIGHT;
+            pins[2].X = x + WIDTH + 20;
+            pins[2].Y = y + HEIGHT / 2 + 2;
+        }
+    }
+}
+
---
+++ Circuits/OrGate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circuits
+{
+    public class OrGate:Gate
+    {
+        public OrGate(int x, int y)
+        {
+
+            //Add the two input pins to the gate
+            pins.Add(new Pin(this, true, 20));
+            pins.Add(new Pin(this, true, 20));
+            //Add the output pin to the gate
+            pins.Add(new Pin(this, false, 20));
+            //move the gate and the pins to the position passed in
+            MoveTo(x, y);
+        }
+
+        public override void Draw(Graphics paper)
+        {
+            //Check if the gate has been selected
+            if (selected)
+            {
+                paper.DrawImage(Properties.Resources.OrGateRed, Left, Top);
+            }
+            else
+            {
+                paper.DrawImage(Properties.Resources.OrGate, Left, Top);
+            }
+            //Draw each of the pins
+            foreach (Pin p in pins)
+                p.Draw(paper);
+        }
+
+        //please make this work savvy savinu it gotta be different for each gate
+        public void MoveTo(int x, int y)
+        {
+            //Debugging message
+            Console.WriteLine("pins = " + pins.Count);
+            //Set the position of the gate to the values passed in
+            left = x;
+            top = y;
+            // must move the pins too
+            pins[0].X = x - GAP;
+            pins[0].Y = y + GAP;
+            pins[1].X = x - GAP;
+            pins[1].Y = y + OrGate.HEIGHT;
+            pins[2].X = x + OrGate.WIDTH + 500;
+            pins[2].Y = y + OrGate.HEIGHT / 2 + 2;
+        }
+
+    }
+
+    
+
+}
